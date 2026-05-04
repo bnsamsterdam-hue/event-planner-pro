89612ede1cafadde8fead9836d5f706683a67e09
The Plee's To Bee <bns.amsterdam@gmail.com>
2026-05-04 19:23:26 +0100

    Update firebase-push-config.js

--- firebase-push-config.js
+++ firebase-push-config.js
@@ -5,4 +5,4 @@
    Firebase Console:
    Projectinstellingen > Cloud Messaging > Web Push certificates > Generate key pair
 */
-window.BNS_PUSH_VAPID_KEY = "VUL_HIER_VAPID_KEY_IN";
+window.BNS_PUSH_VAPID_KEY = "BHuekmgY1oXA47M1TiyCHtWi5NEMgeV2WOUpr7sIpFsan3AyQ9rRaeseHJb-TNkBir6_l6tSHCrPDmVWeM5CWJo";
