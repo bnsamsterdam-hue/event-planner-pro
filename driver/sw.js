f57f0950895452b5aa4b97f40ae4a371f89d091d
The Plee's To Bee <bns.amsterdam@gmail.com>
2026-05-13 22:50:07 +0200

    Add files via upload

--- driver/sw.js
+++ driver/sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', function(e){ self.skipWaiting(); });
-self.addEventListener('activate', function(e){
-  e.waitUntil((async function(){
-    try { var keys = await caches.keys(); await Promise.all(keys.map(function(k){ return caches.delete(k); })); } catch(e) {}
-    try { await self.clients.claim(); } catch(e) {}
-  })());
-});
-self.addEventListener('fetch', function(e){ return; });
+self.addEventListener('install',e=>{self.skipWaiting();});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});self.addEventListener('fetch',e=>{});
