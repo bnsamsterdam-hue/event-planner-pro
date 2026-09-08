4afbd04c621468264e19e1412174c0682d21d2a4
The Plee's To Bee <bns.amsterdam@gmail.com>
2026-09-08 18:48:57 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,10 @@
    ========================================================= */
 
 window.BNS_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCqA0WMgFfn887og0ceRaVDooNnEEa0SEI",
+  apiKey: "AIzaSyCqA0WMgFfn887ogOceRaVDooNnEEa0SEI",
   authDomain: "event-planner-pro-bbcdc.firebaseapp.com",
   projectId: "event-planner-pro-bbcdc",
   storageBucket: "event-planner-pro-bbcdc.firebasestorage.app",
   messagingSenderId: "343572783519",
-  appId: "1:343572783519:web:2c82d09f8901141c1f1daa"
+  appId: "1:343572783519:web:7fd4995fe012c8b51f1daa"
 };
